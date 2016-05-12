92e54f89be530023e56e2d8718bedf1f27f73077
wangjie <jie21st@users.noreply.github.com>
2016-05-12 08:35:05 +0000

    Update: 修改支付单号规则

--- Common/Service/PayService.class.php
+++ Common/Service/PayService.class.php
@@ -62,7 +62,7 @@ class PayService
     public function makePaySn()
     {
         return date('YmdHis')
-            .sprintf('%06d', mt_rand(1, 999999));
+            .sprintf('%04d', mt_rand(1, 999999));
     }
 
     /**
