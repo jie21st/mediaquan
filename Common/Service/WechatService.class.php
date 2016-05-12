5eaf5e31027b52dc8d45581d223d6fa1fc6a7e7f
wangjie <jie21st@users.noreply.github.com>
2016-05-12 02:45:48 +0000

    Update: 微信服务类

---
+++ Common/Service/WechatService.class.php
@@ -0,0 +1,58 @@
+<?php
+namespace Common\Service;
+
+/**
+ * 微信服务类
+ */
+class WechatService
+{
+    protected $wechat;
+    
+    public function __construct()
+    {
+        $this->wechat = new \Org\Util\Wechat;
+    }
+    
+    /**
+     * 生成带参数的二维码
+     *
+     */
+    public function getQRUrl($scene_id, $type = 'QR_SCENE', $expire = 3600)
+    {
+        if ($scene_id <= 0) {
+            return false; 
+        }
+        $result= $this->wechat->getQRCode($scene_id, $type, $expire);
+        if ($result) {
+            $ticket = $result['ticket']; 
+            return $this->wechat->getQRUrl($ticket);
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 公众平台权限
+     * 获取公众平台TOKEN
+     *
+     * @access public
+     * @return 
+     */
+    public function getAccessTokenOp()
+    {
+        $result = $this->wechat->checkAuth();
+        if ($result) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 新增临时素材
+     */
+    public function uploadMedia($data, $type)
+    {
+        return $this->wechat->uploadMedia($data, $type);
+    }
+}
