a86adb1a7a02524b2028768aa583486bc00b7dfb
wangjie <jie21st@users.noreply.github.com>
2016-05-16 10:49:44 +0000

    Update: 个人中心

--- Media/Action/MyAction.class.php
+++ Media/Action/MyAction.class.php
@@ -13,7 +13,40 @@ class MyAction extends \Media\Action\CommonAction
         $userService = new \Common\Service\UserService;
         $userInfo = $userService->getUserFullInfo(session('user_id'));
         
+        $userModel = new \Common\Model\UserModel;
+        $userInfo['fans_num'] = $userModel->where(['parent_id' => session('user_id')])->count();
         $this->assign('user_info', $userInfo);
+        $this->display();
+    }
+    
+    /**
+     * 我的订单
+     */
+    public function orderOp()
+    {
+        $orderService = new \Common\Service\OrderService;
+        $orderModel = new \Common\Model\OrderModel;
+        $condition = array();
+        $condition['buyer_id'] = session('user_id');
+        
+        $orderList = $orderModel->getOrderList($condition);
+        foreach ($orderList as &$order) {
+            $order['state_desc'] = orderState($order);
+            $order['payment_name'] = orderPaymentName($order['payment_code']);
+            
+            //显示取消订单
+            $order['if_cancel'] = $orderService->getOrderOperateState('buyer_cancel', $order);
+            
+            //显示去听课
+            $order['if_learn'] = $orderService->getOrderOperateState('learn', $order);
+            
+            //如果有在线支付且未付款的订单则显示合并付款链接
+            if ($order['order_state'] == ORDER_STATE_NEW) {
+                $order['pay_amount'] = $order['order_amount'];
+            }
+        }
+        
+        $this->assign('order_list', $orderList);
         $this->display();
     }
     
