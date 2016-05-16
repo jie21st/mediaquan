b2dc7ef429e75ec8b4cad1c58a1c950d4b8f03b3
wangjie <jie21st@users.noreply.github.com>
2016-05-16 10:12:28 +0000

    Update: 个人中心页面

--- Media/Action/MyAction.class.php
+++ Media/Action/MyAction.class.php
@@ -5,12 +5,62 @@ class MyAction extends \Media\Action\CommonAction
 {
     protected $needAuth = true;
     
+    /**
+     * 个人中心主页
+     */
     public function indexOp()
     {
         $userService = new \Common\Service\UserService;
         $userInfo = $userService->getUserFullInfo(session('user_id'));
         
         $this->assign('user_info', $userInfo);
+        $this->display();
+    }
+    
+    /**
+     * 我的东家
+     * 
+     */
+    public function parentOp()
+    {
+        $userModel = new \Common\Model\UserModel();
+        $userInfo = $userModel->getUserInfo(['user_id' => session('user_id')]);
+        $parentId = $userInfo['parent_id'];
+        
+        $parentInfo = $userModel->getUserInfo(['user_id' => $parentId]);
+        $this->assign('parent_info', $parentInfo);
+        $this->display();
+    }
+    
+    /**
+     * 我的粉丝
+     */
+    public function fansOp()
+    {
+        $userModel = new \Common\Model\UserModel();
+        $condition = array();
+        $condition['parent_id'] = session('user_id');
+        
+        $count = $userModel->where($condition)->count();
+        $userList = $userModel->where($condition)->select();
+        
+        $this->assign('count', $count);
+        $this->assign('user_list', $userList);
+        $this->display();
+    }
+    
+    /**
+     * 我的课程
+     */
+    public function courseOp()
+    {
+        $classModel = new \Common\Model\ClassModel();
+        $applyList = $classModel->getClassUserList(['user_id' => session('user_id')]);
+        $classIds = array();
+        foreach ($applyList as &$apply) {
+            $apply['class_info'] = $classModel->getClassInfo(['class_id' => $apply['class_id']]);
+        }
+        $this->assign('apply_list', $applyList);
         $this->display();
     }
 }
