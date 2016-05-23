aba3a1ff8e551b1b3d5b785511ab18057f903152
wangjie <jie21st@users.noreply.github.com>
2016-05-23 11:14:34 +0000

    Update: 微信接口测试

--- Static/resource/js/jquery.wechat.js
+++ Static/resource/js/jquery.wechat.js
@@ -2,47 +2,69 @@
     'use strict';
     
     $.wechat = {
-        share: function(options) {
-            $.ajax({
-                url: '/wechat/getJsSign',
-                type: 'GET',
-                data: {
-                    url: encodeURIComponent(window.location.href)
-                },
-                dataType: 'jsonp',
-                jsonp: 'callback',
-                success: function (res) {
-                    if (res.code == 1) {
-                        var params = res.data;
-                        wx.config({
-                            debug: false,
-                            appId: params.appId,
-                            timestamp: params.timestamp,
-                            nonceStr: params.nonceStr,
-                            signature: params.signature,
-                            jsApiList: [
-                                'hideOptionMenu',
-                                'showOptionMenu',
-                                'hideMenuItems',
-                                'showMenuItems',
-                                'hideAllNonBaseMenuItem',
-                                'showAllNonBaseMenuItem',
-                                'closeWindow',
-                                'chooseWXPay',
-                                'onMenuShareAppMessage',
-                                'onMenuShareTimeline',
-                                'onMenuShareQQ',
-                                'onMenuShareWeibo',
-                                'onMenuShareQZone'
-                            ]
-                        });
-                        wx.onMenuShareAppMessage(options)
-                        wx.onMenuShareTimeline(options)
-                    } else {
-                        console.log('初始化wx类失败');
+        isRequest: false,
+        signData: {},
+        init: function(callback)
+        {
+            var self = this;
+            if (isRequest === false) {
+                $.ajax({
+                    url: '/wechat/getJsSign',
+                    type: 'GET',
+                    data: {
+                        url: encodeURIComponent(window.location.href)
+                    },
+                    dataType: 'jsonp',
+                    jsonp: 'callback',
+                    success: function (res) {
+                        if (res.code == 1) {
+                            self.signData = res.data;
+                            wx.config({
+                                debug: false,
+                                appId: params.appId,
+                                timestamp: params.timestamp,
+                                nonceStr: params.nonceStr,
+                                signature: params.signature,
+                                jsApiList: [
+                                    'hideOptionMenu',
+                                    'showOptionMenu',
+                                    'hideMenuItems',
+                                    'showMenuItems',
+                                    'hideAllNonBaseMenuItem',
+                                    'showAllNonBaseMenuItem',
+                                    'closeWindow',
+                                    'chooseWXPay',
+                                    'onMenuShareAppMessage',
+                                    'onMenuShareTimeline',
+                                    'onMenuShareQQ',
+                                    'onMenuShareWeibo',
+                                    'onMenuShareQZone'
+                                ]
+                            });
+                            callback(res.data);
+                        }
+                        
+                        self.isRequest = true;
                     }
-                }
-            });  
+                });
+            } else {
+                callback(this.signData);
+            }
+        },
+        share: function(options) {
+            this.init(function(params){
+                wx.ready(function(){
+                    wx.onMenuShareAppMessage(options);
+                    wx.onMenuShareTimeline(options);
+                })
+            })
+        },
+        hideOptionMenu: function() {
+            this.init(function(){
+                wx.ready(function(){
+                    wx.hideOptionMenu();
+                })
+            })
         }
     }
    
