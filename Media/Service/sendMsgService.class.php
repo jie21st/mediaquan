cdb2eefed74c8adae170ea68f05ec94e6b497854
www <jie21st@users.noreply.github.com>
2016-05-24 10:13:13 +0000

    Update:图文图片

--- Media/Service/sendMsgService.class.php
+++ Media/Service/sendMsgService.class.php
@@ -31,35 +31,40 @@ class sendMsgService
     public function sendNews($userInfo)
     {
         $url = C('RESOURCE_SITE_URL');
-        //$userInfo = D('User', 'Service')->getUserBaseInfo($uid);
+        $name = ($userInfo['user_truename']) ? $userInfo['user_truename'] : $userInfo['user_nickname'];
+        $parentInfo = D('User', 'Service')->getUserBaseInfo($userInfo['parent_id']);
+        $parentName = ($parentInfo['user_truename']) ? $parentInfo['user_truename'] : $parentInfo['user_nickname'];
+        if($parentName == '') {
+            $parentName = $name;
+        }
         $data = [
             'touser' => $userInfo['user_wechatopenid'],
             'msgtype' => 'news',
             'news'  => [
                 'articles' => [
                     [
-                        "title"=>"测试",
-                        "description"=>"建设中, 请勿购买",
+                        "title"=>"欢迎".$name."光临拇指微课",
+                        "description"=>"欢迎".$name."光临拇指微课",
                         "url"=>"http://wangjie.guanlizhihui.com",
-                        "picurl"=> $url . "/image/k1.jpg"
+                        "picurl"=> $url . "/image/k2.jpg"
                     ],
                     [
-                        "title"=>"测试",
-                        "description"=>"建设中, 请勿购买",
+                        "title"=>"拇指微课自动操作流程",
+                        "description"=>"拇指微课自动操作流程",
                         "url"=>"http://wangjie.guanlizhihui.com",
-                        "picurl"=> $url . "/image/k1.jpg"
+                        "picurl"=> $url . "/image/xs.jpg"
                     ],
                     [
-                        "title"=>"测试",
-                        "description"=>"建设中, 请勿购买",
+                        "title"=>"微信运营理论与实操课程",
+                        "description"=>"微信运营与实操课程",
                         "url"=>"http://wangjie.guanlizhihui.com",
-                        "picurl"=> $url . "/image/k1.jpg"
+                        "picurl"=> $url . "/image/k3.jpg"
                     ],
                     [
-                        "title"=>"测试",
-                        "description"=>"建设中, 请勿购买",
+                        "title"=>"去逛逛".$parentName."家的微店",
+                        "description"=>"去逛逛".$parentName."家的微店",
                         "url"=>"http://wangjie.guanlizhihui.com",
-                        "picurl"=> $url . "/image/k1.jpg"
+                        "picurl"=> C('UPLOADS_SITE_URL') . "/avatar/" . $userInfo['user_avatar']
                     ],
                 ]
             ]
