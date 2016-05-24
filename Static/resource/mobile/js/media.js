ee789502234af674ec47a18f2ad26d765dbe0b09
wangjie <jie21st@users.noreply.github.com>
2016-05-24 02:36:10 +0000

    Update: 微信接口测试

---
+++ Static/resource/mobile/js/media.js
@@ -0,0 +1,13 @@
+(function($){
+    /*微信设置*/
+    $.wechat.hideMenuItems([
+        'menuItem:share:qq',
+        'menuItem:share:weiboApp',
+        'menuItem:share:QZone',
+        'menuItem:copyUrl',
+        'menuItem:openWithSafari',
+        'menuItem:openWithQQBrowser'
+    ])
+})(window.Zepto || window.jQuery);
+
+
