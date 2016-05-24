70c9e6e07923d8c54d60f46ab64a3b4e6989295b
wangjie <jie21st@users.noreply.github.com>
2016-05-24 02:45:34 +0000

    Update: 微信接口测试

--- Static/resource/mobile/js/media.js
+++ Static/resource/mobile/js/media.js
@@ -8,7 +8,19 @@
         'menuItem:copyUrl',
         'menuItem:openWithSafari',
         'menuItem:openWithQQBrowser'
-    ])
+    ]);
+    
+    // 页脚菜单固定
+    resizeWindow();
+    $(window).resize(resizeWindow);
+    function resizeWindow()
+    {
+        if ($(document).height() <= $(window).height()) {
+            $('.footer').addClass('guding');
+        } else {
+            $('.footer').removeClass('guding');
+        }
+    }
 })(window.Zepto || window.jQuery);
 
 
