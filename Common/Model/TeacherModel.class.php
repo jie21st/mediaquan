6e49cff6567a2822d160c69d651d7765c3fc955c
wangjie <jie21st@users.noreply.github.com>
2016-06-28 08:17:41 +0000

    Update: 修改数据表名

--- Common/Model/TeacherModel.class.php
+++ Common/Model/TeacherModel.class.php
@@ -3,6 +3,8 @@ namespace Common\Model;
 
 class TeacherModel extends CommonModel
 {
+    protected $trueTableName = 'store_teacher';
+    
     public function getTeacherInfo($condition)
     {
         return $this->where($condition)->find();
