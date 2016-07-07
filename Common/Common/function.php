220784b242cfc0666f30d44c076f7727c4257321
wangjie <jie21st@users.noreply.github.com>
2016-07-07 09:31:33 +0000

    Update: 新增方法

--- Common/Common/function.php
+++ Common/Common/function.php
@@ -550,3 +550,32 @@ function getFiles($path, $allowFiles, &$files = array())
 function str_exists($string, $find) {
 	return !(strpos($string, $find) === false);
 }
+
+/**
+ * 判断一个字符串是否属于序列化后的数据
+ * 
+ * @param type $data
+ * @return boolean
+ */
+function is_serialized($data) {
+     $data = trim( $data );
+     if ( 'N;' == $data )
+         return true;
+     if ( !preg_match( '/^([adObis]):/', $data, $badions ) )
+         return false;
+     switch ( $badions[1] ) {
+         case 'a' :
+         case 'O' :
+         case 's' :
+             if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) )
+                 return true;
+             break;
+         case 'b' :
+         case 'i' :
+         case 'd' :
+             if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) )
+                 return true;
+             break;
+     }
+     return false;
+ }
