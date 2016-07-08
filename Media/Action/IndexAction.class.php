6d3fd070d18859b791f6fda95b734f020546b1cb
wangjie <jie21st@users.noreply.github.com>
2016-07-08 05:31:40 +0000

    Update: 已显示为店铺全部课程

--- Media/Action/IndexAction.class.php
+++ Media/Action/IndexAction.class.php
@@ -15,27 +15,27 @@ class IndexAction extends CommonAction
     /**
      * 首页
      */   
-    public function indexOp()
-    {
-        $classModel = new \Common\Model\ClassModel();
-        $chapterModel = new \Common\Model\ChapterModel;
-        $classService = new \Common\Service\ClassService();
-        
-        $field = 'class_id,class_title,class_image,teacher_id,teacher_name,class_price,study_num';
-        $classList = $classModel->getClassOnlineList([], $field, 'class_sort desc');
-        foreach ($classList as &$classInfo) {
-            // 课程是否已购买
-            $classInfo['is_buy'] = $classService->checkClassUser($classInfo['class_id'], session('user_id'));
-            
-            // 课程章节数
-            $classInfo['chapter_num'] = $chapterModel->getCourseCount([
-                'class_id' => $classInfo['class_id'],
-                'status' => 1,
-            ]);
-        }
-        $this->assign('class_list', $classList);
-        $this->display();
-    }
+//    public function indexOp()
+//    {
+//        $classModel = new \Common\Model\ClassModel();
+//        $chapterModel = new \Common\Model\ChapterModel;
+//        $classService = new \Common\Service\ClassService();
+//        
+//        $field = 'class_id,class_title,class_image,teacher_id,teacher_name,class_price,study_num';
+//        $classList = $classModel->getClassOnlineList([], $field, 'class_sort desc');
+//        foreach ($classList as &$classInfo) {
+//            // 课程是否已购买
+//            $classInfo['is_buy'] = $classService->checkClassUser($classInfo['class_id'], session('user_id'));
+//            
+//            // 课程章节数
+//            $classInfo['chapter_num'] = $chapterModel->getCourseCount([
+//                'class_id' => $classInfo['class_id'],
+//                'status' => 1,
+//            ]);
+//        }
+//        $this->assign('class_list', $classList);
+//        $this->display();
+//    }
     
     /**
      * 联系我们页面
